8d8a274c7980c0af78c67481c7e6acab53327b7a
Andreas Heimann <andreas@iklikla.de>
2019-07-01 22:06:16 +0200

    random generation, three tiles, drawing tiles, moving canvas, zooming canvas

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,25 +5,26 @@ let map = [];
 
 // Settings
 let spriteSize = 32;
-let canvasWidth = 3 * spriteSize;
-let canvasHeight = 3 * spriteSize;
+let canvasWidth = 25;
+let canvasHeight = 25;
 let canvasCenterX = canvasWidth / 2;
 let canvasCenterY = canvasHeight / 2;
+let noiseStrength = 11;
 
-// Tiles
-let tiles = [new Image(), new Image()];
+// Create tiles array with empty images
+let tiles = [new Image(), new Image(), new Image()];
 
 // Setup Canvas
 function init(){
-    console.log("Setup");
+    console.log("Init");
 
     // Load Tileset
     loadTileset(0);
 
     // Generate first World
-    map = generateMap(0);
+    map = generateMap();
 
-    // Draw Map
+    // Draw the map after the last tile loaded successfully
     tiles[tiles.length - 1].addEventListener('load', function() {
         drawMap();
     });
@@ -32,25 +33,26 @@ function init(){
 function loadTileset(tileset = 0) {
     tiles[0].src = 'assets/img/tiles/' + tileset + '/0.png';
     tiles[1].src = 'assets/img/tiles/' + tileset + '/1.png';
+    tiles[2].src = 'assets/img/tiles/' + tileset + '/2.png';
 }
 
 // Draw Map
 function drawMap() {
-    console.log("Draw Map");
-    console.log("Canvas Dimensions: " + canvasWidth + "x" + canvasHeight);
+    console.log("Draw Map - Canvas Dimensions: " + canvasWidth + "x" + canvasHeight);
 
     // Setup Canvas
-    canvas.width = canvasWidth;
-    canvas.height = canvasHeight;
+    canvas.width = canvasWidth * spriteSize;
+    canvas.height = canvasHeight * spriteSize;
 
-    // Loop through map
-    map.forEach(function(xRow, x) {
-        xRow.forEach(function(tile, y) {
-            drawTile(tile, x, y);
-        });
-    })
+    // Clear Canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     // Draw Tiles
+    map.forEach(function(yRow, y) {
+        yRow.forEach(function(tile, x) {
+            drawTile(tile, x, y);
+        });
+    });
 }
 
 function drawTile(tile, x, y) {
@@ -60,19 +62,64 @@ function drawTile(tile, x, y) {
 function loadMap(width, height, mapData) {
     console.log("Loading Map");
 
-    canvasWidth = width * spriteSize;
-    canvasHeight = height * spriteSize;
+    // Reset dimensions
+    canvasWidth = width;
+    canvasHeight = height;
 
+    // Redraw map with new data
     map = mapData;
     drawMap();
 }
 
 // Generate random Worldmap
-function generateMap(seed = null) {
+function generateMap(mode = null) {
     console.log("Generating Map");
 
-    // TODO: Random Generation;
-    return [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+    let newMap = [];
+    let noise = new SimplexNoise();
+
+    // Looping through x and y
+    let x;
+    for(x = 0; x < canvasWidth; x++) {
+        let y;
+
+        // Create an empty array to fill
+        newMap[x] = [];
+
+        for(y = 0; y < canvasHeight; y++) {
+            let noiseValue;
+
+            // Map table
+            // 0 - Water
+            // 1 - Sand
+            // 2 - Grass
+            switch(mode) {
+                default:
+                    // Generate a noise value for this specific coordinate
+                    noiseValue = noise.noise2D(x / noiseStrength, y / noiseStrength);
+
+                    if(noiseValue < 0.2 && noiseValue > -0.2) {
+                        newMap[x][y] = 1;
+                    } else if(noiseValue < -0.2) {
+                        newMap[x][y] = 2;
+                    } else {
+                        newMap[x][y] = 0;
+                    }
+                    break;
+                case "water":
+                    // "water" is just a map with water
+                    newMap[x][y] = 0;
+                    break;
+            }
+        }
+    }
+
+    return newMap;
+}
+
+function drawOnMap(x, y, newTile) {
+    map[x][y] = newTile;
+    drawMap();
 }
 
 // Let's get this party started!
--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,8 +1,97 @@
+let UICanvas = document.querySelector("#mainCanvas");
 let UIDebugMapSizeX = document.querySelector(".debug_mapSizeX");
 let UIDebugMapSizeY = document.querySelector(".debug_mapSizeY");
-
 let UIDebugMapData = document.querySelector(".debug_mapData");
+let UIDebugMapScale = document.querySelector(".debug_mapScale");
+
+let DebugMapScale = 1;
+let DebugMapOffsetX = 0;
+let DebugMapOffsetY = 0;
+let DebugMapMouseOffsetX = 0;
+let DebugMapMouseOffsetY = 0;
+let UIDebugDrawTile = 1;
+let UIDebugStateDraw = false;
+let UIDebugStateMove = false;
 
 function UIDebugLoadMap() {
     loadMap(UIDebugMapSizeX.value, UIDebugMapSizeY.value, JSON.parse(UIDebugMapData.value));
+}
+function UIDebugRandomMap() {
+    loadMap(UIDebugMapSizeX.value, UIDebugMapSizeY.value, []);
+    let newMap = generateMap( Date.now() );
+    loadMap(UIDebugMapSizeX.value, UIDebugMapSizeY.value, newMap);
+    UIDebugMapData.value = JSON.stringify( newMap );
+}
+function UIDebugWaterMap() {
+    loadMap(UIDebugMapSizeX.value, UIDebugMapSizeY.value, []);
+    let newMap = generateMap("water");
+    loadMap(UIDebugMapSizeX.value, UIDebugMapSizeY.value, newMap);
+    UIDebugMapData.value = JSON.stringify( newMap );
+}
+function UIResizeMap() {
+    DebugMapScale = UIDebugMapScale.value;
+    UICanvas.style.transform = "scale(" + DebugMapScale + ") translate(" + DebugMapOffsetX + "px," + DebugMapOffsetY + "px)";
+}
+function UIMoveMap(x, y) {
+    DebugMapOffsetX = x;
+    DebugMapOffsetY = y;
+    UICanvas.style.transform = "scale(" + DebugMapScale + ") translate(" + DebugMapOffsetX + "px," + DebugMapOffsetY + "px)";
+}
+function UIDebugChangeTile(tile) {
+    UIDebugDrawTile = tile;
+}
+
+// Drawing on Canvas
+canvas.addEventListener('mousedown', function(e) {
+    if(e.which === 1) {
+        UIDebugStateDraw = true;
+        document.body.style.cursor = "move";
+    }
+
+    if(e.which === 2) {
+        UIDebugStateMove = true;
+        document.body.style.cursor = "move";
+
+        DebugMapMouseOffsetX = e.pageX;
+        DebugMapMouseOffsetY = e.pageY;
+    }
+});
+canvas.addEventListener('mouseup', function(e) {
+    if(e.which === 1) {
+        UIDebugStateDraw = false;
+        document.body.style.cursor = "default";
+    }
+
+    if(e.which === 2) {
+        UIDebugStateMove = false;
+        document.body.style.cursor = "default";
+    }
+});
+canvas.addEventListener('mousemove', function(e) {
+    if(UIDebugStateDraw) {
+        draw(e);
+    }
+
+    if(UIDebugStateMove) {
+        let x = (e.pageX - UICanvas.offsetLeft) - (UICanvas.width / 2);
+        let y = (e.pageY - UICanvas.offsetTop) - (UICanvas.height / 2);
+
+        console.log("MoveX: " + DebugMapOffsetX + "->" + x);
+        console.log("MoveY: " + DebugMapOffsetY + "->" + y);
+
+        UIMoveMap(x, y);
+    }
+});
+canvas.addEventListener('click', function(e) {
+    draw(e);
+});
+function draw(e) {
+    let canvasXPos = e.pageX;
+    let canvasYPos = e.pageY;
+
+    let x = Math.floor((canvasXPos - UICanvas.offsetLeft) / spriteSize);
+    let y = Math.floor((canvasYPos - UICanvas.offsetTop) / spriteSize);
+
+    drawOnMap(y, x, UIDebugDrawTile);
+    UIDebugMapData.value = JSON.stringify( map );
 }
