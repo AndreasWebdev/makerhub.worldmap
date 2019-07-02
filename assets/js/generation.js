604e79d3495923b68f7f2704180a47f526b4052d
Andreas Heimann <andreas@iklikla.de>
2019-07-02 14:14:17 +0200

    added noise falloff

--- assets/js/generation.js
+++ assets/js/generation.js
@@ -1,5 +1,10 @@
+// Settings
+let noiseStrength = 15;
+let noiseFalloff = 0;
+let noiseFalloffStrength = 1.5;
+
 // Generate random Worldmap
-function generateMap(width = 25, height = 25, mode = null, noiseStrength = 5) {
+function generateMap(width = 25, height = 25, mode = null, noiseStrength = 11) {
     console.log("Generating Map");
 
     let newMap = [];
@@ -15,6 +20,7 @@ function generateMap(width = 25, height = 25, mode = null, noiseStrength = 5) {
 
         for(y = 0; y < canvasHeight; y++) {
             let noiseValue;
+            let noiseValueCombined;
 
             // Map table
             // 0 - Water
@@ -23,14 +29,37 @@ function generateMap(width = 25, height = 25, mode = null, noiseStrength = 5) {
             switch(mode) {
                 default:
                     // Generate a noise value for this specific coordinate
-                    noiseValue = noise.noise2D(x / noiseStrength, y / noiseStrength);
+                    noiseFalloff = furthestDistanceFromCentre - euclideanDistance({x, y}, centrePoint) * noiseFalloffStrength;
+                    noiseValue = clamp(noise.noise2D(x / noiseStrength, y / noiseStrength), 0, 1);
+                    noiseValueCombined = clamp(noiseValue / 2 * noiseFalloff, 0, 1);
 
-                    if(noiseValue < 0.2 && noiseValue > -0.2) {
-                        newMap[x][y] = 1;
-                    } else if(noiseValue < -0.2) {
-                        newMap[x][y] = 2;
-                    } else {
-                        newMap[x][y] = 0;
+                    switch(true) {
+                        case(noiseValueCombined < 0.2):
+                            newMap[x][y] = 0;
+                            break;
+                        case(noiseValueCombined < 1):
+                            newMap[x][y] = 1;
+                            break;
+                        default:
+                            newMap[x][y] = 2;
+                            break;
+                    }
+                    break;
+                case "debug":
+                    noiseFalloff = furthestDistanceFromCentre - euclideanDistance({x, y}, centrePoint) * noiseFalloffStrength;
+                    noiseValue = clamp(noise.noise2D(x / noiseStrength, y / noiseStrength), 0, 1);
+                    noiseValueCombined = clamp(noiseValue / 2 * noiseFalloff, 0, 1);
+
+                    switch(true) {
+                        case(noiseValueCombined < 0.2):
+                            newMap[x][y] = 0;
+                            break;
+                        case(noiseValueCombined < 1):
+                            newMap[x][y] = 0.5;
+                            break;
+                        default:
+                            newMap[x][y] = 1;
+                            break;
                     }
                     break;
                 case "water":
@@ -43,3 +72,20 @@ function generateMap(width = 25, height = 25, mode = null, noiseStrength = 5) {
 
     return newMap;
 }
+
+function clamp(num, min, max) {
+    return num <= min ? min : num >= max ? max : num;
+}
+
+function euclideanDistance(point1, point2) {
+    return Math.sqrt(
+        Math.abs(Math.pow(point1.x - point2.x, 2)) +
+        Math.abs(Math.pow(point1.y - point2.y, 2))
+    )
+}
+
+var centrePoint = {x: Math.floor(canvasWidth / 2), y: Math.floor(canvasHeight / 2)};
+
+var furthestDistanceFromCentre = euclideanDistance(
+    {x: 0, y: 0}, centrePoint
+);
